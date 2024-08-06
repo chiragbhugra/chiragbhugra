1322815f9cd9f58746a88daf84a516228bfa4b65
chirag bhugra <ichiragbhugra@gmail.com>
2024-08-06 15:55:59 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <>
+      <h1 className="text-3xl font-bold underline">hello world </h1>
+      <p>helooooo</p>
+      
+    </>
+    
+  )
+}
+
+export default App
